df644d9fd8c0e35a06892c4995941c22e123ec72
Akash RP <akashrp512@gmail.com>
2023-01-14 09:22:31 +0530

    loginScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-   
-    <HomeScreen />
+    <BrowserRouter>
+      {!user ? (<LoginScreen />): (
+        <Routes>
+          <Route path='/' element={<HomeScreen />} />
+        </Routes>
+      )}
+    </BrowserRouter>
     </div>
   );
 }
