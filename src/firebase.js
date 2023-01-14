53ab686684ad6ae0b170fd2113ae28eb977b8e41
Akash RP <akashrp512@gmail.com>
2023-01-14 11:43:50 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from './firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyC_8yKLkKNwtreIItvFH1RYJGHa2YsdSMc",
     authDomain: "netflix-clone-f8189.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "689132083030",
     appId: "1:689132083030:web:9a9a83b93bb62a1689e8f7"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.fireStore();
+  const auth =firebase.auth();
+
+  export {auth};
+  export default db;
